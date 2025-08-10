e9352ca5aa8c48db072a72e4ad8595269cc8e1f9
sadman sakib <sadmansakib1075@gmail.com>
2025-08-10 23:38:17 +0600

    added password hashing

--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { createUserZodSchema } from "./user.validation";
 
 export const userRoutes = Router()
 
 
-userRoutes.post("/register", userController.createUser)
+userRoutes.post("/register", validateRequest(createUserZodSchema),userController.createUser)
--- src/app/module/user/user.service.ts
+++ src/app/module/user/user.service.ts
@@ -1,8 +1,11 @@
 import { IUser } from "./user.interface";
 import { User } from "./user.model";
+import bcryptjs from "bcryptjs"
 
-const createUser = async (payload: IUser) => {
-    console.log(payload);
+
+const createUser = async (payload: IUser) => {  
+    const hashpassword=await bcryptjs.hash(payload.password as string,10)
+    payload.password=hashpassword;
     const create = await User.create(payload)
     return create
 }
