e71feff8bca508152d3cddc04b94cbcf72d198f0
sadman sakib <sadmansakib1075@gmail.com>
2025-08-11 20:29:54 +0600

    added access token

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -5,12 +5,14 @@ dotenv.config()
 interface EnvConfig {
     PORT: string,
     DB_URL: string,
-    NODE_ENV: "development" | "production"
+    NODE_ENV: "development" | "production",
+    JWT_ACCESS_EXPIRES:string,
+    JWT_ACCESS_SECRET:string
 }
 
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV"]
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV","JWT_ACCESS_EXPIRES","JWT_ACCESS_SECRET"]
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
             throw new Error(`Missing Required environment variable ${key}`)
@@ -20,7 +22,9 @@ const loadEnvVariables = (): EnvConfig => {
     return {
         PORT: process.env.PORT as string,
         DB_URL: process.env.DB_URL!,
-        NODE_ENV: process.env.NODE_ENV as "development" | "production"
+        NODE_ENV: process.env.NODE_ENV as "development" | "production",
+        JWT_ACCESS_EXPIRES:process.env.JWT_ACCESS_EXPIRES as string,
+        JWT_ACCESS_SECRET:process.env.JWT_ACCESS_SECRET as string
     }
 
 }
--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { IUser } from "../user/user.interface"
 import { User } from "../user/user.model"
 import httpstatus from "http-status-codes"
 import bcryptjs from "bcryptjs"
+import { generateToken } from "../../utils/jwt"
+import { envVars } from "../../config/env"
 
 const credetialsLogin = async (payload: Partial<IUser>) => {
     const { email, password } = payload
@@ -16,9 +18,18 @@ const credetialsLogin = async (payload: Partial<IUser>) => {
         throw new AppError(httpstatus.BAD_REQUEST, "Incorrect Password")
     }
 
-    
+    const jwtPayload = {
+        userId: isUserExist._id,
+        email: isUserExist.email,
+        role: isUserExist.role
+    }
+
+    const accessToken = await generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
+
+
     return {
-        email: isUserExist.email
+        email: isUserExist.email,
+        accessToken: accessToken
     }
 }
 
