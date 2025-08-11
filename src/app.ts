55d1dd025b2fee143825db67368a0de93309799f
sadman sakib <sadmansakib1075@gmail.com>
2025-08-11 12:54:13 +0600

    added auth routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import { userRoutes } from "./app/module/user/user.route";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import { notFoundRoute } from "./app/middlewares/notFoundRoutes";
+import { authRoutes } from "./app/module/auth/auth.route";
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(cors())
 
 
 app.use("/api/v1/user", userRoutes)
+app.use("/api/v1/auth", authRoutes)
 
 app.get("/", async (req, res) => {
     res.send("server is running man")
