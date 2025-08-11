71e352539a0cb1de2ae331d8154b04a84711930b
sadman sakib <sadmansakib1075@gmail.com>
2025-08-11 12:51:56 +0600

    added auth controller

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -1,22 +1,25 @@
 import { NextFunction, Request, Response } from "express"
 import httpstatus from "http-status-codes"
 import { sendResponse } from "../../utils/sendResponse"
-const credetialsLogin=async(req:Request,res:Response,next:NextFunction)=>{
-try {
-    
-         sendResponse(res, {
+import { authService } from "./auth.service"
+const credetialsLogin = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const loginInfo = await authService.credetialsLogin(req.body)
+
+        sendResponse(res, {
             success: true,
             statusCode: httpstatus.CREATED,
             message: "user login successfully",
-            data: create
+            data: loginInfo
         })
-} catch (error) {
-    next(error)
+    } catch (error) {
+        next(error)
+    }
 }
-}
 
 
 
-export const authController={
+export const authController = {
     credetialsLogin
 }
