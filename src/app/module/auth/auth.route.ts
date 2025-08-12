76be9aa99e548e990f72fccef99e874c50dc7238
sadman sakib <sadmansakib1075@gmail.com>
2025-08-12 19:30:43 +0600

    added new access token routes

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -5,3 +5,4 @@ import { authController } from "./auth.controller";
 export const authRoutes = Router()
 
 authRoutes.post("/login", authController.credetialsLogin)
+authRoutes.post("/refresh-token", authController.getNewAccessToken)
