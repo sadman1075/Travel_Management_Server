817fd77c9afbd9b86dac77264fafc152fe80298f
sadman sakib <sadmansakib1075@gmail.com>
2025-08-13 20:46:19 +0600

    added division consent

---
+++ src/app/module/division/division.service.ts
@@ -0,0 +1,59 @@
+import { IDivision } from "./division.interface";
+import { Division } from "./division.model";
+
+const createDivision = async (payload: IDivision) => {
+
+    const existingDivision = await Division.findOne({ name: payload.name });
+    if (existingDivision) {
+        throw new Error("A division with this name already exists.");
+    }
+
+
+    // const baseSlug = payload.name.toLowerCase().split(" ").join("-")
+    // let slug = `${baseSlug}-division`
+
+    // let counter = 0;
+    // while (await Division.exists({ slug })) {
+    //     slug = `${slug}-${counter++}` // dhaka-division-2
+    // }
+
+    // payload.slug = slug;
+
+    const division = await Division.create(payload);
+
+    return division
+};
+
+const getSingleDivision = async (slug: string) => {
+    const division = await Division.findOne({ slug });
+    return {
+        data: division,
+    }
+};
+
+const getAllDivisions = async (query: Record<string, string>) => {
+
+    const queryBuilder = new QueryBuilder(Division.find(), query)
+
+    const divisionsData = queryBuilder
+        .search(divisionSearchableFields)
+        .filter()
+        .sort()
+        .fields()
+        .paginate()
+
+    const [data, meta] = await Promise.all([
+        divisionsData.build(),
+        queryBuilder.getMeta()
+    ])
+
+    return {
+        data,
+        meta
+    }
+};
+
+export const divisionService={
+    createDivision,
+    getSingleDivision
+}
