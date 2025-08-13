9d00ab9ae626ba8d8631401dc935fc98fad6f39d
sadman sakib <sadmansakib1075@gmail.com>
2025-08-13 10:59:59 +0600

    google authentication configuration

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,3 +1,41 @@
-import passport from "pass"
+import passport, { Profile } from "passport"
+import { Strategy as GoogleStrategy, VerifyCallback } from "passport-google-oauth20"
+import { envVars } from "./env"
+import { User } from "../module/user/user.model";
+import { Role } from "../module/user/user.interface";
 
-passport.use()
+passport.use(
+    new GoogleStrategy({
+        clientID: envVars.GOOGLE_CLIENT_ID,
+        clientSecret: envVars.GOOGLE_CLIENT_SECRET,
+        callbackURL: envVars.GOOGLE_CALLBACK_URL
+    }, async (accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) => {
+        try {
+            const email=profile.emails?.[0].value;
+            if(!email){
+                return done(null,false,{message:"No email found"})
+            }
+
+            let user=await User.findOne({email})
+            if(!user){
+                user=await User.create({
+                    email,
+                    name:profile.displayName,
+                    picture:profile.photos?.[0].value,
+                    role:Role.USER,
+                    isVerified:true,
+                    auths:[{
+                        provider:"google",
+                        providerId:profile.id
+                    }]
+                })
+            }
+
+            return done(null,user)
+
+
+        } catch (error) {
+            done(error)
+        }
+    })
+)
