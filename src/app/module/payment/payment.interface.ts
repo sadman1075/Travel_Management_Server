ddeeb5fcc9d47864a7b7d4c902ef21a233c42567
sadman sakib <sadmansakib1075@gmail.com>
2025-08-22 09:46:34 +0600

    added payment model

---
+++ src/app/module/payment/payment.interface.ts
@@ -0,0 +1,21 @@
+import { Types } from "mongoose";
+
+export enum PAYMENT_STATUS{
+    PAID="PAID",
+    UNPAID="UNPAID",
+    CANCEL="CANCELLTED",
+    FAILED="FAILED",
+    REFUNDED="REFUNDED"
+}
+
+export interface IPayment{
+    booking:Types.ObjectId
+    transactionId:string
+    amount:number
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    paymentGatewayData?:any
+    invoiceUrl?:string
+    status:PAYMENT_STATUS
+
+
+}
