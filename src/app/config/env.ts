0b5adf3804e90e7960443e98997a6c044504c4b6
sadman sakib <sadmansakib1075@gmail.com>
2025-08-26 11:27:31 +0600

    added email sender config

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -36,6 +36,13 @@ interface EnvConfig {
         CLOUDINARY_API_KEY: string;
         CLOUDINARY_API_SECRET: string;
     };
+    EMAIL_SENDER: {
+        SMTP_USER: string;
+        SMTP_PASS: string;
+        SMTP_PORT: string;
+        SMTP_HOST: string;
+        SMTP_FROM: string;
+    };
 
 
 }
@@ -56,7 +63,12 @@ const loadEnvVariables = (): EnvConfig => {
         "CLOUDINARY_CLOUD_NAME",
         "CLOUDINARY_API_KEY",
         "CLOUDINARY_API_SECRET",
-        // " SSL_IPN_URL"
+        // " SSL_IPN_URL",
+        "SMTP_PASS",
+        "SMTP_PORT",
+        "SMTP_HOST",
+        "SMTP_USER",
+        "SMTP_FROM",
     ]
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
@@ -100,6 +112,14 @@ const loadEnvVariables = (): EnvConfig => {
             CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
             CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
         },
+        EMAIL_SENDER: {
+            SMTP_USER: process.env.SMTP_USER as string,
+            SMTP_PASS: process.env.SMTP_PASS as string,
+            SMTP_PORT: process.env.SMTP_PORT as string,
+            SMTP_HOST: process.env.SMTP_HOST as string,
+            SMTP_FROM: process.env.SMTP_FROM as string,
+        },
+
 
     }
 
